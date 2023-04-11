d4404e94866291dcfa9d801c1291def509a6d4af
subhan300 <subhan.akram2400@gmail.com>
2023-04-12 03:24:12 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import "./App.css";
 import Layout from "./pages/customer-dashboard/Layout";
 import AdminLayout from "./pages/admin-dashboard/AdminLayout";
 import {
-  BrowserRouter,
-  Link,
   Route,
   Routes,
-  redirect,
   useNavigate,
 } from "react-router-dom";
 import Login from "./pages/auth/Login";
